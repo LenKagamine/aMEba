c66aa4c69d9ac05cd85f31ef3964faa6e0e14fa9
Gordon-Winchester <gordonwinch@gmail.com>
2015-01-21 17:32:29 -0500

    God Mode pt. 6
    added enemies but they just wobble atm

--- GodLevel.java
+++ GodLevel.java
@@ -5,6 +5,7 @@ public class GodLevel{
   private Button[] btns;
   private boolean buttonActivated;
   private int cursorType;
+  private int enemyLevel, enemySpecies;
   private ArrayList e;
   private ArrayList berries;
   private ArrayList rocks;
@@ -17,15 +18,18 @@ public class GodLevel{
     HEIGHT = map.getHeight();
     btns = new Button[]{
       new Button(GamePanel.WIDTH/10+50,50,100,50,"Rock"),
-      new Button(GamePanel.WIDTH*2/10+50,50,100,50,"Berry")
+      new Button(GamePanel.WIDTH*2/10+50,50,100,50,"Berry"),
+      new Button(GamePanel.WIDTH*3/10+50,50,100,50,"Enemy")
     };
     cursorType = -1;
     /*cursorType governs what is selected
      * -1 = blank
      * 0 = rock
      * 1 = berry
+     * 2 = spawn enemy
      */
-    //new changes whee
+    enemyLevel = 1;
+    enemySpecies = 0;
     e = new ArrayList();
     berries = new ArrayList();
     rocks = new ArrayList();
@@ -39,39 +43,41 @@ public class GodLevel{
       if(en.isDead()){
         e.remove(i);
       }
-      for(int j=0;j<rocks.size();j++){
-        if (en.getBoxRect().intersects(((Rock)rocks.get(j)).getBoxRect()))
-          en.collide();
-      }
-      en.setFollow(false);
-      for(int j=0;j<e.size();j++){ //enemies attack each other
-        if(en.getSpecies() != ((Enemy)e.get(j)).getSpecies()){
-          if(en.insight(((Enemy)e.get(j)).getScreenPos())){
-            en.setTarget(((Enemy)e.get(j)).getScreenPos());
-            en.setFollow(true);
-            en.attack(((Enemy)e.get(j)));
-            if(((Enemy)e.get(j)).isDead()){
-              en.consume((((Enemy)e.get(j)).getDNA()));
-              e.remove(j);
-            }
-          }
-          else{
+      else{
+        for(int j=0;j<rocks.size();j++){
+          if (en.getBoxRect().intersects(((Rock)rocks.get(j)).getBoxRect()))
+            en.collide();
+        }
+        en.setFollow(false);
+        for(int j=0;j<e.size();j++){ //enemies attack each other
+          if(en.getSpecies() != ((Enemy)e.get(j)).getSpecies()){
             if(en.insight(((Enemy)e.get(j)).getScreenPos())){
               en.setTarget(((Enemy)e.get(j)).getScreenPos());
               en.setFollow(true);
-              en.mate((Enemy)e.get(j));
+              en.attack(((Enemy)e.get(j)));
+              if(((Enemy)e.get(j)).isDead()){
+                en.consume((((Enemy)e.get(j)).getDNA()));
+                e.remove(j);
+              }
+            }
+            else{
+              if(en.insight(((Enemy)e.get(j)).getScreenPos())){
+                en.setTarget(((Enemy)e.get(j)).getScreenPos());
+                en.setFollow(true);
+                en.mate((Enemy)e.get(j));
+              }
             }
           }
         }
-      }
-      for(int j=0;j<berries.size();j++){ //enemies eat berry
-        /*if(en.insight(((Berry)berries.get(j)).getScreenPos())){
-         en.setTarget(((Berry)berries.get(j)).getScreenPos());
-         en.setFollow(true);
-         }*/
-        if(((Berry)berries.get(j)).getRect().intersects(en.getBoxRect())){
-          en.consume((Berry)berries.get(j));
-          berries.remove(j);
+        for(int j=0;j<berries.size();j++){ //enemies eat berry
+          /*if(en.insight(((Berry)berries.get(j)).getScreenPos())){
+           en.setTarget(((Berry)berries.get(j)).getScreenPos());
+           en.setFollow(true);
+           }*/
+          if(((Berry)berries.get(j)).getRect().intersects(en.getBoxRect())){
+            en.consume((Berry)berries.get(j));
+            berries.remove(j);
+          }
         }
       }
     }
@@ -112,6 +118,9 @@ public class GodLevel{
         break;
       case 1:
         berries.add(new Berry(map,mx+map.getX(),my+map.getY()));
+        break;
+      case 2:
+        e.add(new Enemy(map,mx+map.getX(),my+map.getY(),enemySpecies,enemyLevel));
         break;
     }
   }
