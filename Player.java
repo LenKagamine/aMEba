7d792547eaf8b9e6c3db2c5dde10e4ed1218923a
Jasonator <Jasonator@users.noreply.github.com>
2015-01-22 00:16:09 -0500

    comments

--- Player.java
+++ Player.java
@@ -11,20 +11,20 @@ public class Player extends Organism{
 	}
 	public void update(){
 		super.update();
-		angle = Math.atan2(y-mapy-my,x-mapx-mx)+Math.PI;
-		prevx = x;
+		angle = Math.atan2(y-mapy-my,x-mapx-mx)+Math.PI; //finds angle
+		prevx = x; //keeps prev location of player
 		prevy = y;
 
-		x += speed*Math.cos(angle);
+		x += speed*Math.cos(angle); //moves player based on speed and direction
 		if(x<width/2) x = width/2;
 		if(x>Levels.WIDTH-width/2) x = Levels.WIDTH-width/2;
 		y += speed*Math.sin(angle);
 		if(y<height/2) y = height/2;
 		if(y>Levels.HEIGHT-height/2) y = Levels.HEIGHT-height/2;
 
-		attacking = false;
+		attacking = false; //now not attacking
 	}
-	public void draw(Graphics2D g){
+	public void draw(Graphics2D g){ //draws player
 		super.draw(g);
 		g.setColor(Color.black);
 		g.fillRoundRect((int)(GamePanel.WIDTH/4)-5,(int)(GamePanel.HEIGHT-100)-5,(int)(GamePanel.WIDTH/2)+10,50+10,20,20);
@@ -33,21 +33,21 @@ public class Player extends Organism{
 		g.setColor(Color.yellow);
 		g.drawString((int)health+"/"+(int)dna.getHealth(),(int)(GamePanel.WIDTH/2),(int)(GamePanel.HEIGHT-100)+25);
 	}
-	public void mouse(int mx,int my){
+	public void mouse(int mx,int my){ //gets location
 		this.mx = mx;
 		this.my = my;
 	}
-	public void click(int mx,int my){
+	public void click(int mx,int my){ //if clicked... player attacks
 		attacking = true;
 	}
 	public boolean isAttacking(){
 		return attacking;
 	}
-	public void collide(){
+	public void collide(){ //prevents player from moving into rocks
 		x = prevx;
 		y = prevy;
 	}
-	public double getHealth(){
+	public double getHealth(){ //returns current health
 		return health;
 	}
 }
