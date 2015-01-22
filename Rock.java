a4e460c9de016b3eabadeb575262a344632f0856
Gordon-Winchester <gordonwinch@gmail.com>
2015-01-22 00:43:13 -0500

    Commented Rock

--- Rock.java
+++ Rock.java
@@ -2,24 +2,24 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 public class Rock extends MapObject{
-	private static BufferedImage img;
-	public Rock(Map map,double x,double y){
-		super(map,x,y);
-		try{
-			img = ImageIO.read(getClass().getResourceAsStream("rock.png"));
-			boxwidth = width = img.getWidth();
-			boxheight = height = img.getHeight();
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	public void draw(Graphics2D g){
-		mapx = map.getX();
-		mapy = map.getY();
-		if(x-mapx+width/2 > 0 && y-mapy+height/2 > 0 && x-mapx-width/2 < GamePanel.WIDTH && y-mapy-height/2 < GamePanel.HEIGHT)
-			g.drawImage(img,(int)(x-mapx-width/2),(int)(y-mapy-height/2),width,height,null);
-	}
-	public boolean checkStuck(double x, double y){
-		return (x >= this.x-80 && x <= this.x+80 && y >= this.y-80 && y <= this.y+80);
-	}
+ private static BufferedImage img;
+ public Rock(Map map,double x,double y){
+  super(map,x,y);
+  try{ // create image and set dimensions
+   img = ImageIO.read(getClass().getResourceAsStream("rock.png"));
+   boxwidth = width = img.getWidth();
+   boxheight = height = img.getHeight();
+  } catch(Exception e){
+   e.printStackTrace();
+  }
+ }
+ public void draw(Graphics2D g){ // draw onto map if within bounds of field of view
+  mapx = map.getX();
+  mapy = map.getY();
+  if(x-mapx+width/2 > 0 && y-mapy+height/2 > 0 && x-mapx-width/2 < GamePanel.WIDTH && y-mapy-height/2 < GamePanel.HEIGHT)
+   g.drawImage(img,(int)(x-mapx-width/2),(int)(y-mapy-height/2),width,height,null);
+ }
+ public boolean checkStuck(double x, double y){ // check if something is stuck in the rock
+  return (x >= this.x-80 && x <= this.x+80 && y >= this.y-80 && y <= this.y+80);
+ }
 }
