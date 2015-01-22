51692f05e09860c668b59c19b7738cf6895b7235
Jasonator <Jasonator@users.noreply.github.com>
2015-01-22 00:11:54 -0500

    comments

--- Berry.java
+++ Berry.java
@@ -5,16 +5,16 @@ public class Berry extends MapObject{
 	private int recover;
 	public Berry(Map map,double x, double y){
 		super(map,x,y);
-		if(Math.random()*15 < 1){
+		if(Math.random()*15 < 1){ //chance to become superberry
 			recover = 120;
 			try{
-				img = ImageIO.read(getClass().getResourceAsStream("superberry.png"));
+				img = ImageIO.read(getClass().getResourceAsStream("superberry.png")); //get image
 			} catch(Exception e){
 				e.printStackTrace();
 			}
 		}
 		else{
-			recover = 20;
+			recover = 20; //recover health
 			try{
 				img = ImageIO.read(getClass().getResourceAsStream("berry.png"));
 			} catch(Exception e){
@@ -24,14 +24,14 @@ public class Berry extends MapObject{
 		width = img.getWidth();
 		height = img.getHeight();
 	}
-	public void draw(Graphics2D g){
+	public void draw(Graphics2D g){ //draws berry
 		mapx = map.getX();
 		mapy = map.getY();
 
 		if(x-mapx+width/2 > 0 && y-mapy+height/2 > 0 && x-mapx-width/2 < GamePanel.WIDTH && y-mapy-height/2 < GamePanel.HEIGHT)
 			g.drawImage(img, (int)(x-mapx-width/2), (int)(y-mapy-height/2), null);
 	}
-	public int recoverHealth(){
+	public int recoverHealth(){ //returns amount to recover
 		return recover;
 	}
 }
